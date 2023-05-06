ca0cbc89232dd0eee02b277388034c3c63d776a1
Yossec <1313178@senati.pe>
2023-05-06 18:46:57 -0500

    primer ejercicio

---
+++ primer_ejercicio.R
@@ -0,0 +1,8 @@
+
+my_name <- readline(prompt="Ingrese su nombre: ")
+my_age  <- readline(prompt="Ingrese su edad en años: ")
+
+print(paste("Tu nombre es: ,", my_name, 
+            "y tu edad es:", 
+            my_age 
+            ))
