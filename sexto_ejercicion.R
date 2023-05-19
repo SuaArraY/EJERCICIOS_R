7c5b45854b501c31eac32af54b9c75c66d4a073b
SuaArraY <115846093+SuaArraY@users.noreply.github.com>
2023-05-18 20:01:15 -0500

    sexto ejercicio, programa de dos dimensiones secuencia mayores de 50

---
+++ sexto_ejercicion.R
@@ -0,0 +1,11 @@
+
+
+
+a <- seq(50,78, by=2) #se almacena en una variable la secuencia de 2 en 2, que comienza de 50, asta el 78
+
+# en la variable se crea la matix para insertar la scuencia
+# en la matrix (secuencia , numero de filas, numero de columnas)
+
+# en la matrix en la parte de la secuencia inserte la variable y me devuelve lo q solicita el ejercicio
+M <- matrix(a, nrow = 5, ncol=3 )
+print(M)
